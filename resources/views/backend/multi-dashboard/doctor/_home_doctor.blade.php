d60bcf204e3bf2abce6ff00265cccf6ed4850c1f
MaheKarim <mahekarim@gmail.com>
2020-09-22 01:22:46 +0600

    Bad Decesion

--- resources/views/backend/multi-dashboard/doctor/_home_doctor.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('backend.multi-dashboard.doctor._layout_doctor')
-
-@section('content')
-
-
-
-
-@endsection
--- vendor/haruncpi/laravel-log-reader/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-Route::group(['namespace' => 'Haruncpi\LaravelLogReader\Controllers', 'middleware' => ['web','auth']], function () {
-
-    Route::get(config('laravel-log-reader.view_route_path'), 'LogReaderController@getIndex');
-    Route::post(config('laravel-log-reader.view_route_path'), 'LogReaderController@postDelete');
-    Route::get(config('laravel-log-reader.api_route_path'), 'LogReaderController@getLogs');
-
-});
